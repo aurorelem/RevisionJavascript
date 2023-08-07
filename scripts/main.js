f1d5dc13185cce7d2da18eb4467322f076e4f1b5
Aurore <aurorelemaire.pro@gmail.com>
2023-08-07 13:54:19 +0200

    changeur d'image javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Bonjour le monde !";
+/*let myHeading = document.querySelector("h1");
+myHeading.textContent = "Bonjour le monde !";*/
 /* Pour faire un commentaire en JS*/
 //C'est pareil qu'en CSS
+
+let myImage = document.querySelector("img");
+
+myImage.addEventListener("click", function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "Images/javascript.png") {
+    myImage.setAttribute("src", "Images/javascript2.png");
+  } else {
+    myImage.setAttribute("src", "Images/javascript.png");
+  }
+});
